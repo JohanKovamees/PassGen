a8c9c74c7695ac28115eb6200bad627192066c93
JohanKovamees <64374183+JohanKovamees@users.noreply.github.com>
2020-07-13 15:02:30 +0200

    Add files via upload

--- Passgenerator.cpp
+++ Passgenerator.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
-#include <array>
+#include <vector>
+#include <conio.h>
+#include <stdio.h>
+#include <process.h>
+
+
 
 using namespace std;
+
 
 int randomSelAll() { //Generates a random Char
 	int r = 33;
@@ -35,30 +41,77 @@ int randomSelSym() { //Generates a random symbol
 	return r;
 }
 
-void printA(const std::array<int, 16>& arr) {
-	for (int n = 0; n < arr.size(); n++) {
-		std::cout << (char)arr[n];
+void printA(const vector<int>& vec) { //Prints the Vector
+	
+	std::cout << "\n\n\n\tYour Generated Password: \n\n\t";
+	for (int n = 0; n < vec.size(); n++) {
+		std::cout << (char)vec[n];
 	}
+	std::cout << "\n";
 }
 
-
-int main() {	
-	
+vector<int> createPc() {
+	int length;
+	cout << "\n\tEnter password length: \n";
+	cin >> length;
+	vector<int> vec;
+	vec.resize(length);
 	srand(time(NULL));
-//	cout << "Enter password length: ";
-//	cin >> length;
 
-	std::array <int, 16> arr;
-
-
-
-	for (int i = 0; i < arr.size(); i = i + 1) {
-		arr[i] = randomSelAll();
+	for (int i = 0; i < vec.size(); i = i + 1) {
+		vec[i] = randomSelAll();
 	}
 
-	std::cout << "Your Generated Password: ";
+	return vec;
+}
 
-	printA(arr);
+vector<int> createP(int length) {
+	vector<int> vec;
+	vec.resize(length);
+	srand(time(NULL));
+
+	for (int i = 0; i < vec.size(); i = i + 1) {
+		vec[i] = randomSelAll();
+	}
+
+	return vec;
+}
+
+int main() {	
+
+	int s = 0;
+	do
+	{
+		
+		cout << "\n\n\n\t Password Options";
+		cout << "\n\n\t01. Length: 64";
+		cout << "\n\n\t02. Length: 128";
+		cout << "\n\n\t03. Length: 512";
+		cout << "\n\n\t04. Length: Custom";
+		cout << "\n\n\t05. Exit";
+		cout << "\n\n\tSelect Option (1-5) ";
+		_getch();
+		cin >> s;
+		switch (s)
+		{
+		case 1:
+			printA(createP(64));
+			break;
+		case 2:
+			printA(createP(128));
+			break;
+		case 3:
+			printA(createP(512));
+			break;
+		case 4:
+			printA(createPc());
+			break;
+		case 5:
+			exit(0);
+		default:
+			cout << "\a";
+		}
+	} while (s != '5');
 
 
 
